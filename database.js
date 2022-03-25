77a04c54e92b2f398bc498120ed1f030f6081fa8
CristopherSa9 <luiski_cr7@hotmail.com>
2022-03-24 18:52:29 -0600

    Update

--- database.js
+++ database.js
@@ -1,9 +1,9 @@
 let config = {
-	host: "localhost",
-	user: "root",
-	password: "",
-	port: 3306,
-	database: "adoption_system",
-	secret_key: "H773gfuyfh78wd7g"
+    host: "us-cdbr-east-05.cleardb.net",
+    user: "bc8364ebb9ef3c",
+    password: "3cc7c094",
+    port: 3306,
+    database: "heroku_755eb81efd60190",
+    secret_key: "qwertyuiopasdfghjklñzxcvbnM4"
 };
 exports.config = config;
