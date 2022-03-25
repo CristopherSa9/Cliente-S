9c7594c795711bcb3958f9fce4660a8d88a37a9f
CristopherSa9 <luiski_cr7@hotmail.com>
2022-03-24 19:07:43 -0600

    primer commit

--- database.js
+++ database.js
@@ -1,9 +1,9 @@
 let config = {
-    host: "us-cdbr-east-05.cleardb.net",
-    user: "bc8364ebb9ef3c",
-    password: "3cc7c094",
-    port: 3306,
-    database: "heroku_755eb81efd60190",
-    secret_key: "qwertyuiopasdfghjklñzxcvbnM4"
+	host: "us-cdbr-east-05.cleardb.net",
+	user: "ba5300e4d974af",
+	password: "e913cea1",
+	port: 3306,
+	database: "heroku_8a8e65c7440b65a",
+	secret_key:"ashryputrtlopa1jki"
 };
 exports.config = config;
